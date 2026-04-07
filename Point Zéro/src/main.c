05108bc7ce9235e28bec86ee1909693e1776d7c5
olehdrahomiretskyi <ioleg.olrksandr@gmail.com>
2026-04-07 09:52:30 +0200

    Update main.c

--- Point Zéro/src/main.c
+++ Point Zéro/src/main.c
@@ -35,7 +35,7 @@ int main(int argc,char* argv[]){
     (void)argc;(void)argv;
     SDL_Init(SDL_INIT_VIDEO); TTF_Init();
     srand((unsigned)time(NULL));
-    SDL_Window*   win=SDL_CreateWindow("Bataille Navale v4",
+    SDL_Window*   win=SDL_CreateWindow("Point Zéro",
                          SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_W,WINDOW_H,
                          SDL_WINDOW_FULLSCREEN_DESKTOP);
     SDL_Renderer* ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
